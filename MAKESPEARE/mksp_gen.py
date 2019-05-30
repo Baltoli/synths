0a20e892568d9fa54d069d5caed793da95c75abb
Bruce Collie <brucecollie82@gmail.com>
2019-05-30 13:16:30 +0100

    Work on generating

--- MAKESPEARE/mksp_gen.py
+++ MAKESPEARE/mksp_gen.py
@@ -43,7 +43,7 @@ def flatten(l):
 #   r2 is the first valid index for the next array (i.e. size)
 #   r0 is the number of arrays - in my case 2 for now?
 
-def to_raw(ret, inps, outp, test=False):
+def to_raw(inps, outp, ret=None, test=False):
     len0 = len(inps[0])
     assert(all(map(lambda i: len(i) == len0, inps)))
 
@@ -52,20 +52,43 @@ def to_raw(ret, inps, outp, test=False):
         'r7': 0, 'r6': len0 - 1, 'r2': len0,
         'r0': len(inps), 'r8': 0, 'r9': 0,
         'input_mem': flatten(inps),
-        'sc_return_flag': 0, 'sc_return_val': ret,
+        'sc_return_flag': 0 if ret is None else 1, 
+        'sc_return_val': ret if ret is not None else 0,
         'output_mem_start': 0, 'output_mem': outp
     }
 
-def add_one(n):
-    ins = [random.randint(-1E3, 1E3) for _ in range(n)]
-    outs = list(map(lambda x: x + 1, ins))
-    return (0, [ins], outs)
+def random_input(n):
+    return [random.randint(-1E3, 1E3) for _ in range(n)]
 
-def many(gen_f, n, max_len, with_test = True):
-    ret = [gen_f(random.randint(0, max_len)) for _ in range(max_len)]
-    if with_test:
-        ret.append(gen_f(max_len * 10))
-    return [format_example(to_raw(*r)) for r in ret]
+def add_one(n):
+    ins = random_input(n)
+    outs = list(map(lambda x: x + 1, ins))
+    return ([ins], outs, None)
+
+def ident(n):
+    ins = random_input(n)
+    return ([ins], ins, None)
+
+def prod(n):
+    ins_a = random_input(n)
+    ins_b = random_input(n)
+    outs = [a*b for a,b in zip(ins_a, ins_b)]
+    return ([ins_a, ins_b], outs, None)
+
+def dot(n):
+    ins_a = random_input(n)
+    ins_b = random_input(n)
+    out = sum([a*b for a,b in zip(ins_a, ins_b)])
+    return ([ins_a, ins_b], [], out)
+
+def many(gen_f, n, max_len, test_p = 0.3):
+    train_n = int((1 - test_p) * n)
+    test_n = int(test_p * n)
+
+    train = [format_example(to_raw(*gen_f(random.randint(0, max_len)))) for _ in range(train_n)]
+    test = [format_example(to_raw(*gen_f(random.randint(0, max_len*5)), test=True)) for _ in range(test_n)]
+
+    return train + test
 
 def write_input_file(name, gen_f, n, max_len = 20):
     with open(name, 'w') as f:
@@ -77,4 +100,4 @@ def write_input_file(name, gen_f, n, max_len = 20):
             writer.writerow(r)
 
 if __name__ == "__main__":
-    write_input_file('test.tsv', add_one, 50)
+    write_input_file('test.tsv', prod, 2200)
